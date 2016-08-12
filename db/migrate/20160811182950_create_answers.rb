5d87f278544a504116b1793965978bdea96424db
Denis.B <hawk911@yandex.ru>
2016-08-12 22:20:28 +0300

    Исправления

--- db/migrate/20160811182950_create_answers.rb
+++ db/migrate/20160811182950_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
   def change
     create_table :answers do |t|
-      t.belongs_to :question, index: true
+      t.belongs_to :question, index: true,f oreign_key: true
       t.text :body
 
       t.timestamps
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of :body }
   it { should validate_presence_of :question_id }
+  it { should belong_to :question }
 end
