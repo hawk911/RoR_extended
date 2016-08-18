5d128a60d288cd2c17401122aa4c4b14469a1033
Denis.B <hawk911@yandex.ru>
2016-08-18 07:59:26 +0300

    create rspec answer

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,2 +1,36 @@
 class AnswerController < ApplicationController
+  before_action :load_question, only: [:index, :show, :create]
+
+  def index
+    @answer =@question.answers
+  end
+
+  def show
+    @answer = @question.answers.find(params[:id])
+
+  end
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      redner :new
+    end
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
 end
--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe AnswerController, type: :controller do
 
+  let(:question) { create(:question)  }
+  let(:answer) { create(:answer, question: question) }
+
+  describe 'GET #index' do
+
+    let(:answers) { create_list(:answer, 2, question: question) }
+
+    before {get :index, params: {question_id: :question}}
+
+    it 'status 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'populates an array of all answers' do
+      expect(assigns(:answers)).to match_array(answers)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
 end
