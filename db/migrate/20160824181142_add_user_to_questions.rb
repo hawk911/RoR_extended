55623ff5e4a1e3c36370635c1622f09e7e8db4ba
Denis.B <hawk911@yandex.ru>
2016-08-25 22:52:40 +0300

    must know

--- db/migrate/20160824181142_add_user_to_questions.rb
+++ db/migrate/20160824181142_add_user_to_questions.rb
@@ -2,5 +2,6 @@ class AddUserToQuestions < ActiveRecord::Migration[5.0]
   def change
     add_column :questions,:user_id, :integer
     add_index :questions, :user_id
+    #add_reference :questions, :user, index: true, foreign_key: true
   end
 end
--- db/migrate/20160824182058_add_user_to_answers.rb
+++ db/migrate/20160824182058_add_user_to_answers.rb
@@ -2,5 +2,6 @@ class AddUserToAnswers < ActiveRecord::Migration[5.0]
   def change
     add_column :answers,:user_id, :integer
     add_index :answers, :user_id
+    #add_reference :answers, :user, index: true, foreign_key: true
   end
 end
