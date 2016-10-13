8d534ec9f4cce110ef5341de83e3d0cb321004fc
Denis.B <hawk911@yandex.ru>
2016-10-13 09:16:01 +0300

    question i18n

--- spec/feature/questions/edit_question_spec.rb
+++ spec/feature/questions/edit_question_spec.rb
@@ -19,15 +19,17 @@ feature 'User edit question', %q{
 
     scenario 'show Edit' do
       within '.question' do
-        expect(page).to have_content 'Edit'
+        expect(page).to have_content I18n.t('questions.form.edit')
       end
     end
 
     scenario 'edits with valid attributes', js: true do
       within '.question' do
-        click_on 'Edit'
-        fill_in 'Question', with: 'edit question'
-        click_on 'Save'
+        click_on I18n.t('questions.form.edit')
+        fill_in I18n.t('activerecord.models.question'), with: 'edit question'
+        click_on I18n.t('questions.form.edit_save')
+
+        wait_for_ajax
 
         expect(page).to_not have_content question.body
         expect(page).to have_content 'edit question'
@@ -37,32 +39,38 @@ feature 'User edit question', %q{
 
     scenario 'edits with invalid attributes', js: true do
       within '.question' do
-        click_on 'Edit'
-        expect(page).not_to have_content 'Edit'
-        fill_in 'Question', with: ''
-        click_on 'Save'
+        click_on I18n.t('questions.form.edit')
+        expect(page).not_to have_content I18n.t('questions.form.edit')
+        fill_in I18n.t('activerecord.models.question'), with: ''
+        click_on I18n.t('questions.form.edit_save')
 
-        expect(page).to_not have_content 'Edit'
+        wait_for_ajax
+
+        expect(page).to_not have_content I18n.t('questions.form.edit')
         expect(page).to have_selector('textarea')
       end
     end
   end
 
   context 'not owner of the answer' do
-    scenario "can't see edit link" do
+    scenario "can't see edit link", js: true do
       sign_in other_user
       visit question_path question
 
+      wait_for_ajax
+
       within '.question' do
         expect(page).not_to have_selector('textarea')
-        expect(page).not_to have_content('Edit')
+        expect(page).not_to have_content(I18n.t('questions.form.edit'))
       end
     end
   end
 
-  scenario 'Non-Authenticated User edit' do
+  scenario 'Non-Authenticated User edit', js: true do
     visit question_path(question)
 
-    expect(page).to_not have_link 'Edit'
+    wait_for_ajax
+
+    expect(page).to_not have_link I18n.t('questions.form.edit')
   end
 end
