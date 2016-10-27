18ca9d3f0411785bdc609c32790f14ea488dfd00
Denis.B <hawk911@yandex.ru>
2016-10-27 08:40:03 +0300

    add index for attachment_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024033119) do
+ActiveRecord::Schema.define(version: 20161027043558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161024033119) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type", using: :btree
+    t.index ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
   end
 
   create_table "questions", force: :cascade do |t|
