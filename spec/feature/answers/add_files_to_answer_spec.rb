d6c6bb7852c18cc50d934966a569ae8283442b06
Denis.B <hawk911@yandex.ru>
2016-10-27 08:40:34 +0300

    spec answer, files add for answer

---
+++ spec/feature/answers/add_files_to_answer_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../feature_helper'
+
+feature 'Add files to answer', %q{
+  As author answer
+  I'd like to be able to attach files
+  For illustrate
+  } do
+
+context 'Authenticated user' do
+  given(:user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+  scenario 'user create valid answer with file', js: true do
+    fill_in I18n.t('activerecord.attributes.answer.body'), with: 'text answer'
+    save_and_open_page
+    within all('.nested-fields').first do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on I18n.t('answers.form.submit')
+
+    within(".answer") do
+       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+
+  end
+
+  scenario 'user create invalid answer with file' do
+
+  end
+
+  scenario 'user delete file' do
+
+  end
+end
+
+context 'No-Authenticated user' do
+  scenario 'create valid answer with file' do
+
+  end
+end
+
+end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
   context 'association' do
-    it { should belong_to(:question) }
-    #it { should belong_to(:answer) }
+    it { should belong_to(:attachable) }
+    it { should validate_presence_of :file }
   end
 end
