ac2476a792d8479d0526835a6469199c043240ee
Denis.B <hawk911@yandex.ru>
2016-10-27 08:38:53 +0300

    spec question, add files for question

--- spec/feature/questions/add_files_to_question_spec.rb
+++ spec/feature/questions/add_files_to_question_spec.rb
@@ -13,15 +13,28 @@ context 'Authenticated user' do
     sign_in(user)
     visit questions_path
   end
-  scenario 'user create valid question with file' do
+  scenario 'user create valid question with file', js: true do
     click_on I18n.t('questions.index.ask')
     fill_in I18n.t('activerecord.attributes.question.title'), with: 'Test question'
     fill_in I18n.t('activerecord.attributes.question.body'), with: 'Body question'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    within all('.nested-fields').first do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on I18n.t('questions.form.add_file')
+
+    within all('.nested-fields').last do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
     click_on I18n.t('questions.form.submit')
 
+    wait_for_ajax
+
     within(".question") do
-    expect(page).to have_content 'spec_helper.rb'
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
 
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,11 +4,17 @@ RSpec.describe Question, type: :model do
   context 'validate' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
+    it { should validate_presence_of :user_id }
   end
 
   context 'association' do
+    it { should belong_to :user }
     it { should have_many(:answers).dependent(:destroy) }
     it { should have_many(:attachments).dependent(:destroy) }
+  end
+
+  context 'have index' do
+    it { should have_db_index :user_id }
   end
 
   context 'ordered' do
