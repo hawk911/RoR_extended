fb98a804abd6da62073f3fad60672b2719004b6a
Denis.B <hawk911@yandex.ru>
2016-11-07 08:13:20 +0300

    feature question

--- spec/feature/questions/add_files_to_question_spec.rb
+++ spec/feature/questions/add_files_to_question_spec.rb
@@ -6,9 +6,11 @@ feature 'Add files to question', %q{
   For illustrate
   } do
 
+given(:user) { create(:user) }
+given(:question) { create(:question, user: user) }
+given(:other_user) { create(:user) }
+given(:question_attachment) { create(:question_attachment, attachable: question) }
 context 'Authenticated user' do
-  given(:user) { create(:user) }
-
   background do
     sign_in(user)
     visit questions_path
@@ -32,26 +34,72 @@ context 'Authenticated user' do
 
     wait_for_ajax
 
-    within(".question") do
+    within('.question') do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
+  end
+
+  scenario 'user create invalid question with file', js: true do
+    click_on I18n.t('questions.index.ask')
+    fill_in I18n.t('activerecord.attributes.question.title'), with: ''
+    fill_in I18n.t('activerecord.attributes.question.body'), with: 'Body question'
+
+    within all('.nested-fields').last do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
+    click_on I18n.t('questions.form.submit')
+
+    wait_for_ajax
+
+    within '.question_errors' do
+      expect(page).to have_content (I18n.t('activerecord.attributes.question.title') +
+                                      ' ' + I18n.t('errors.messages.blank'))
     end
 
   end
 
-  scenario 'user create invalid question with file' do
+  scenario 'user delete file', js: true do
+    visit question_path(question_attachment)
 
+    expect(page).to have_link I18n.t('questions.form.delete_file')
+
+    within '.question' do
+      expect(page).to have_link 'rails_helper.rb'
+      within '.question_attachment' do
+        click_on I18n.t('questions.form.delete_file')
+      end
+    end
+
+    visit question_path(question_attachment)
+
+    wait_for_ajax
+
+    expect(page).to_not have_link I18n.t('questions.form.delete_file')
   end
+end
 
-  scenario 'user delete file' do
-
+context 'Other user' do
+  background do
+   sign_in(other_user)
+   visit question_path(question_attachment)
+  end
+  scenario 'can not add file', js: true do
+    expect(page).to_not have_link I18n.t('questions.form.add_file')
+  end
+  scenario 'cannot delete file', js: true do
+    expect(page).to_not have_link I18n.t('questions.form.delete_file')
   end
 end
 
 context 'No-Authenticated user' do
-  scenario 'create valid question with file' do
+  scenario 'create valid question with file', js: true do
+    visit question_path(question_attachment)
+    within '.question' do
+      expect(page).to_not have_link I18n.t('questions.form.add_file')
+    end
 
   end
 end
-
 end
