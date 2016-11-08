992326d15c4867f625aec332fa0ec8b44d6fc16c
Denis.B <hawk911@yandex.ru>
2016-11-08 08:35:15 +0300

    refactorin and add

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password) }
+  context 'validate' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
+
+  context 'association' do
+    it { should have_many(:questions) }
+    it { should have_many(:answers) }
+  end
+
 end
