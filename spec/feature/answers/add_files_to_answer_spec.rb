304a4d3553f238868bb53e17b3cd530be6ca43bd
Denis.B <hawk911@yandex.ru>
2016-11-14 08:01:19 +0300

    spec answer

--- spec/feature/answers/add_files_to_answer_spec.rb
+++ spec/feature/answers/add_files_to_answer_spec.rb
@@ -7,9 +7,7 @@ feature 'Add files to answer', "
   " do
 
   given(:user) { create(:user) }
-  given(:question) { create(:question) }
-  #given(:other_user) { create(:user) }
-  #given(:answer_attachment) { create(:answer_attachment, attachable: answer) }
+  given!(:question) { create(:question_with_answers) }
 
   context 'Authenticated user' do
     before do
@@ -18,11 +16,17 @@ feature 'Add files to answer', "
     end
 
     scenario 'user create valid answer with file', js: true do
-      fill_in I18n.t('activerecord.attributes.answer.title'), with: 'text answer'
+
+
       within all('.nested-fields').first do
         attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
       end
-      #save_and_open_page
+      within '.new_answer' do
+      	pry
+      	fill_in 'answer_body', with: 'answer'
+        attach_file('File', File.join(Rails.root, '/spec/spec_helper.rb'))
+      end
+
       click_on I18n.t('questions.form.add_file')
 
       within all('.nested-fields').last do
@@ -40,7 +44,7 @@ feature 'Add files to answer', "
     end
 
     scenario 'user create invalid answer with file', js: true do
-      fill_in I18n.t('activerecord.attributes.answer.body'), with: ''
+      fill_in 'answer_body', with: 'answer'
       within all('.nested-fields').first do
         attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
       end
@@ -59,7 +63,6 @@ feature 'Add files to answer', "
   context 'No-Authenticated user' do
     scenario 'create valid question with file', js: true do
       visit question_path(question)
-      save_and_open_page
       within '.new_answer' do
         expect(page).to_not have_link I18n.t('answers.form.add_file')
       end
