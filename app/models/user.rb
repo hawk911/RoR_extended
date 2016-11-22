2299e8f8b68d797c0139bf2a75b34a97d80df493
Denis.B <hawk911@yandex.ru>
2016-11-22 08:23:03 +0300

    view, specs - vote

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   def author_of?(object)
     object.user_id == id
   end
+
+  def voted?(object)
+    object.votes.where(user: self).exists?
+  end
+
+  def can_vote?(object)
+    !author_of?(object) && !voted?(object)
+  end
+
 end
