fc0038f3edd51e604e921a2d4876cf9302e72520
Denis.B <hawk911@yandex.ru>
2016-12-05 08:55:42 +0300

    actionCable begginer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #ActionCable
+  config.action_cable.allowed_request_origins = ['http://0.0.0.0:3000']
+  config.action_cable.disable_request_forgery_protection = true
 end
