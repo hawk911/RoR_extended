5cfa72f2102df660c93506c50513f92338e19718
Denis.B <hawk911@yandex.ru>
2016-12-15 08:13:57 +0300

    actionCable answer

--- app/assets/javascripts/channels/answers.coffee
+++ app/assets/javascripts/channels/answers.coffee
@@ -1,7 +1,7 @@
 App.answers = App.cable.subscriptions.create channel: "AnswersChannel" ,
   connected: ->
-    console.log "You are connected to 'AnswersChannel'"
     question_id = $(".question").attr('id')
+    console.log "You are connected to 'AnswersChannel' - id:  #{question_id}"
     @perform 'start_stream_answers',  question_id: question_id
 
   disconnected: ->
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -18,4 +18,19 @@ class Answer < ApplicationRecord
       update!(best: !best?)
     end
   end
+
+  #def broadcast
+  #  return if errors.any?
+  #
+  #   files = []
+  #  attachments.each { |a| files << { id: a.id, file_url: a.file.url, file_name: a.file.identifier } }
+  #
+  #   ActionCable.server.broadcast(
+  #    "answers_#{question_id}",
+  #   answer:             self,
+  #  answer_attachments: files,
+  # answer_voting:      votes,
+  #question_user_id:   question.user_id
+  #)
+  #end
 end
