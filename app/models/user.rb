00679879eb6119c1405f04d79b2e11ea1c716793
Denis.B <hawk911@yandex.ru>
2017-01-24 07:37:22 +0300

    settings omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   def author_of?(object)
     object.user_id == id
