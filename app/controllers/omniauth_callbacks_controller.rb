0799d44e8a739c49d7a59af926af6eb43b795a80
Denis.B <hawk911@yandex.ru>
2017-01-31 08:44:29 +0300

    authorization twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,35 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-def facebook
-  @user = User.find_for_oauth(request.env['omniauth.auth'])
-  if @user.persisted?
-    sign_in_and_redirect @user, event: :authentication
-    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+  def facebook
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    end
+
+    def failure
+      redirect_to root_path
+    end
   end
-end
 
-def twitter
-  render json: request.env['omniauth.auth']
-end
+  def twitter
+    #render json: request.env['omniauth.auth']
+    def twitter
+      @user = User.find_for_oauth(request.env["omniauth.auth"])
 
+      if @user.confirmed?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
+      else
+        session[:new_user_id] = @user.id
+        #redirect_to edit_signup_email_path
+        redirect_to edit_user_registration
+      end
+    end
+
+    def failure
+      redirect_to root_path
+    end
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  before_create :skip_confirmation!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable,
+    :recoverable, :rememberable, :trackable, :validatable, :confirmable,
     :omniauthable, omniauth_providers: [:facebook, :twitter]
   has_many :questions
   has_many :answers
-  has_many :authorizations
+  has_many :authorizations, dependent: :destroy
 
   def author_of?(object)
     object.user_id == id
@@ -24,19 +25,34 @@ class User < ApplicationRecord
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
     return authorization.user if authorization
 
-    email = auth.info[:email]
-    user = User.where(email: email).first
-    if user
-      user.create_authorization(auth)
+    if auth.info[:email].present?
+      email = auth.info[:email]
+      user = User.where(email: email).first
     else
+      email = random_email
+      confirmation_required = true
+    end
+
+    if !user
       password = Devise.friendly_token[0,20]
       user = User.create!(email: email, password: password, password_confirmation: password)
-      user.create_authorization(auth)
+      user.update!(confirmed_at: nil) if confirmation_required
     end
+
+    user.create_authorization(auth)
     user
+
   end
 
   def create_authorization(auth)
     self.authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
+
+  def last_authorization_twitter?
+    (authorizations.any? && authorizations.last.provider == 'twitter') ? true : false
+  end
+
+  def self.random_email
+    "#{SecureRandom.hex(15)}@example.com"
   end
 end
