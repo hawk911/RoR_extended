596dd5bd04753c3d6f923fc63e3882a8581d42bb
Denis.B <hawk911@yandex.ru>
2017-02-03 08:38:15 +0300

    setting cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,18 @@ class ApplicationController < ActionController::Base
 
   before_action :set_gon_user, unless: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: exception.message }
+      format.json { render json: { error: 'You are not authorized to perform
+        requested action' }.to_json, status: :forbidden }
+
+      format.js   { head :forbidden }
+    end
+  end
+
+  check_authorization unless: :devise_controller?
+
   private
 
   def set_gon_user
