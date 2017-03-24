da5ad8e13e479e4c9c63a2236887903ed4bdece1
Denis.B <hawk911@yandex.ru>
2017-03-24 08:02:41 +0300

    refactoring for cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,5 +26,15 @@ class Ability
     guest_abilities
     can :create, [Question, Answer, Comment]
     can :update, [Question, Answer, Comment] , user: user
+    can :destroy, [Question, Answer, Comment], user: user
+
+    can :set_best, Answer, question: { user: user }
+
+    can :vote_up, [Question, Answer]
+    can :vote_down, [Question, Answer]
+    cannot :vote_up, [Question, Answer], user: user
+    cannot :vote_down, [Question, Answer], user: user
+
+    can :destroy, [Attachment], attachable: { user: user }
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -21,20 +21,55 @@ describe 'Ability' do
   describe 'for user' do
     let(:user) { create :user }
     let(:other_user) { create :user  }
+    let(:his_question)    { create(:question, user: user) }
+    let(:other_question) { create(:question, user: other_user) }
+    let(:his_answer)      { create(:answer, user: user) }
+    let(:other_answer)   { create(:answer, user: other_user) }
+
     it { should_not be_able_to :manage, :all }
     it { should be_able_to :read, :all }
 
+    # Create
     it { should be_able_to :create, Question }
     it { should be_able_to :create, Answer }
     it { should be_able_to :create, Comment }
 
-    it { should be_able_to :update, create(:question, user: user), user: user }
-    it { should_not be_able_to :update, create(:question, user: other_user), user: user }
+    # Update
+    it { should be_able_to :update, create(:question, user: user) }
+    it { should_not be_able_to :update, create(:question, user: other_user) }
 
-    it { should be_able_to :update, create(:answer, user: user), user: user }
-    it { should_not be_able_to :update, create(:answer, user: other_user), user: user }
+    it { should be_able_to :update, create(:answer, user: user) }
+    it { should_not be_able_to :update, create(:answer, user: other_user) }
 
-    it { should be_able_to :update, create(:comment, user: user), user: user }
-    it { should_not be_able_to :update, create(:comment, user: other_user), user: user }
+    it { should be_able_to :update, create(:comment, user: user) }
+    it { should_not be_able_to :update, create(:comment, user: other_user) }
+
+    # Destroy
+    it { should be_able_to :destroy, his_question }
+    it { should_not be_able_to :destroy, other_question }
+
+    it { should be_able_to :destroy, his_answer }
+    it { should_not be_able_to :destroy, other_answer }
+
+    # Set best answer
+    it { should be_able_to :set_best, create(:answer, question: his_question) }
+    it { should_not be_able_to :set_best, create(:answer, question: other_question) }
+
+     # Votes
+    it { should be_able_to :vote_up, other_question }
+    it { should_not be_able_to :vote_up, his_question }
+
+    it { should be_able_to :vote_down, other_question }
+    it { should_not be_able_to :vote_down, his_question }
+
+    it { should be_able_to :vote_up, create(:answer) }
+    it { should_not be_able_to :vote_up, his_answer }
+
+    it { should be_able_to :vote_down, create(:answer) }
+    it { should_not be_able_to :vote_down, his_answer }
+
+    # Attachments
+    it { should be_able_to :destroy, his_question.attachments.build }
+    it { should_not be_able_to :destroy, other_question.attachments.build }
   end
 end
