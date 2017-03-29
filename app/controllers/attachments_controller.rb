53f95c14ea34900e5920efc2eebd2667c3ac1d50
Denis.B <hawk911@yandex.ru>
2017-03-29 07:46:57 +0300

    refactoring and cancan

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,18 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!, only: [:destroy]
+  before_action :load_attachment, only: [:destroy]
+
+  authorize_resource
+
+  respond_to :js
 
   def destroy
+    respond_with(@attachment.destroy)
+  end
+
+  private
+
+  def load_attachment
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy if current_user.author_of?(@attachment.attachable)
   end
 end
