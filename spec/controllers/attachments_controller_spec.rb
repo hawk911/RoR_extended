54fbb8d743adfa89b8dcf44300eb01e56d9b5135
Denis.B <hawk911@yandex.ru>
2017-03-30 07:20:18 +0300

    add spec in attachments_controller_spec

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,27 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
-  let!(:file) { create(:question_attachment) }
+  let(:another_user) { create :user }
+  let!(:question) { create(:question, user: another_user) }
+  let!(:question_file) { create(:question_attachment) }
+  describe 'Questions' do
+    describe 'DELETE #Destroy' do
+      context 'author answer' do
+        sign_in_user
+        before do
+          attachable = question_file.attachable
+          attachable.update user: @user
+          question_file.reload
+        end
 
-  describe 'DELETE #Destroy' do
-    context 'author answer' do
-      sign_in_user
-      before do
-        attachable = file.attachable
-        attachable.update user: @user
-        file.reload
+        it 'destroy question_file' do
+          expect { delete :destroy, format: :js, params: { id: question_file.id } }.to change(Attachment, :count).by(-1)
+        end
       end
 
-      it 'destroy file' do
-        expect { delete :destroy, format: :js, params: { id: file.id } }.to change(Attachment, :count).by(-1)
+      context 'non-author answer' do
+        sign_in_user
+
+        it 'not destroy question_file' do
+          expect { delete :destroy, format: :js, params: { id: question_file.id } }.to_not change(Attachment, :count)
+        end
       end
     end
+  end
 
-    context 'non-author answer' do
-      sign_in_user
+  describe 'Answers' do
+    let!(:answer) { create(:answer, question: question, user: another_user) }
+    let!(:answer_file) { create(:attachment, attachable: answer) }
+    describe 'DELETE #Destroy' do
+      context 'author answer' do
+        sign_in_user
+        before do
+          attachable = answer_file.attachable
+          attachable.update user: @user
+          answer_file.reload
+        end
 
-      it 'not destroy file' do
-        expect { delete :destroy, format: :js, params: { id: file.id } }.to_not change(Attachment, :count)
+        it 'deletes files' do
+          expect { delete :destroy, format: :js, params: { id: answer_file } }.to change(answer.attachments, :count).by(-1)
+        end
+      end
+
+      context 'non-author answer' do
+        it 'try to delete answer file' do
+          expect { delete :destroy, format: :js, params: { id: answer_file } }.not_to change(Attachment, :count)
+        end
       end
     end
   end
