c12d7639ccec30144eb558cc5d51a675f415251d
Denis.B <hawk911@yandex.ru>
2017-04-02 17:25:45 +0300

    Do api with doorkeeper

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module RoRExtended
     # -- all .rb files in that directory are automatically loaded.
 
     # i18n default ru
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     # Settings generate
     config.generators do |g|
