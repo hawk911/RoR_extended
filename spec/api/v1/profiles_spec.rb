15ad52aad6b8ec572d119d4cd9c14323a87ba948
Denis.B <hawk911@yandex.ru>
2017-04-04 08:10:38 +0300

    lesson 14

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -19,7 +19,8 @@ describe 'Profile API' do
     let(:me) { create(:user) }
     let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-    before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, as: :json }
+    before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
+    #before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, as: :json }
 
     it 'returns 200 status' do
       expect(response).to be_success
@@ -38,4 +39,38 @@ describe 'Profile API' do
     end
   end
 
+  describe 'GET /index' do
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let!(:others) { create_list(:user, 5) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/', format: :json, access_token: access_token.token }
+      #before { get '/api/v1/profiles/', params: { access_token: access_token.token }, as: :json }
+
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'does not contain me' do
+        expect(response.body).to_not include_json(me.to_json)
+      end
+
+      it 'does contain other users' do
+        expect(normalize_json(response.body)).to be_json_eql(normalize_json(others.to_json))
+      end
+
+      %w(id email created_at updated_at admin).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to have_json_path("0/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not captain #{attr}" do
+          expect(response.body).to_not have_json_path("0/#{attr}")
+        end
+      end
+    end
+  end
 end
