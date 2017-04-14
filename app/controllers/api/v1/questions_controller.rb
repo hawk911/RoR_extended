925e96da62c72e9d40acb46a7ffb725abba9dce1
Denis.B <hawk911@yandex.ru>
2017-04-14 08:11:47 +0300

    create api for question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   authorize_resource class: Question
 
   before_action :load_questions, only: [:index, :list]
+  before_action :load_question, only: :show
 
   def index
     respond_with @questions
@@ -19,5 +20,9 @@ class Api::V1::QuestionsController < Api::V1::BaseController
 
   def load_questions
     @questions =Question.all
+  end
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 end
