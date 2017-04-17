0dd80d95d72db83fd6bbbdc2f09eda798a8e4afb
Denis B <hawk911@yandex.ru>
2017-04-17 09:03:10 +0300

    api answers

---
+++ app/serializers/answer_complex_serializer.rb
@@ -0,0 +1,5 @@
+class AnswerComplexSerializer < ActiveModel::Serializer
+  attributes :id, :body, :created_at, :updated_at, :question_id, :user_id
+  has_many :attachments
+  has_many :comments
+end
