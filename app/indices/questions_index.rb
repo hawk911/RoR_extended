4915eadcc733955cf59eaa3fd1dd1ae593924b44
Denis.B <hawk911@yandex.ru>
2017-05-18 09:06:56 +0300

    edit

--- app/indices/questions_index.rb
+++ app/indices/questions_index.rb
@@ -1,9 +1,10 @@
 ThinkingSphinx::Index.define :question, with: :active_record do
-#filed
-indexes title, sortable:true
-indexes body
-indexes user.email, as: author, sortable:true
+  #fileds
+  indexes title, sortable: true
+  indexes body
+  indexes user.email, as: :author, sortable: true
 
-#attributes
-has user_id, created_at, updated_at
+  # attributes
+  has user_id, created_at, updated_at
+  has '1', as: :model_order, type: :integer
 end
