82b558704a3531cdcfda6cb3cb1ee816fe451170
Denis.B <hawk911@yandex.ru>
2017-06-30 07:28:30 +0300

    edit schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # Use this file to easily define all of your cron jobs.
 #
 # Learn more: http://github.com/javan/whenever
-set :bundle_command, "/home/deploy/.rbenv/shims/bundler"
+set :bundle_command, "/home/deploy/.rbenv/shims/"
 set :output, "log/whenever.log"
 
 every 1.day, at: '00:01 am' do
